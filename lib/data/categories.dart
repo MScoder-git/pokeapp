02e3ec2c51b7ecfd9662920536a282ed88b471aa
eliot  alderson <eliotalderson@debian.debian.net>
2019-08-19 20:55:45 +0200

    first commit

---
+++ lib/data/categories.dart
@@ -0,0 +1,11 @@
+import 'package:pokemon/configs/AppColors.dart';
+import 'package:pokemon/models/category.dart';
+
+const List<Category> categories = [
+  Category(name: "Pokedex", color: AppColors.teal),
+  Category(name: "Moves", color: AppColors.red),
+  Category(name: "Abilities", color: AppColors.blue),
+  Category(name: "Items", color: AppColors.yellow),
+  Category(name: "Locations", color: AppColors.purple),
+  Category(name: "Type Charts", color: AppColors.brown),
+];
